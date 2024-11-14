8cfffa4657d368794b9b1a6171f4e07cfb3cf0f1
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2024-11-14 13:40:33 +0000

    Simple prototype of add exercise page, non functional

---
+++ WorkoutTracker/WorkoutTracker/WorkoutTracker/Components/Exercises/ExerciseSet.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkoutTracker.Components.Exercises
+{
+    public class ExerciseSet
+    {
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public int SetNumber { get; set; }
+        public int Reps { get; set; }
+        public int Weight { get; set; }
+    }
+}
